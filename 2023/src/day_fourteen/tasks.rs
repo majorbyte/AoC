3486fb6d5282a1e1f932be445c9f327cc61d8dcb
majorbyte <majorbyte@gmail.com>
2023-12-15 00:31:19 +0100

    Day 14 cleanup - still no pattern search :/

--- 2023/src/day_fourteen/tasks.rs
+++ 2023/src/day_fourteen/tasks.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use itertools::Itertools;
 
 pub fn task() {
 
@@ -28,40 +29,12 @@ pub fn task() {
 
 
 fn task_1(input: &str){
-    let mut patterns: Vec<String> = vec![];
 
-    let lines:Vec<Vec<char>> = input.split("\r\n").map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-    for x in 0..lines[0].len() {
-        let mut y = lines.len();
-        let mut p:Vec<char> = vec![];
-        while y>0{
-            y-=1;
-            p.push(lines[y][x]);
-        }
-        patterns.push(p.into_iter().collect());
-    }
+    let lines:Vec<String> = input.split("\r\n").map(|l| l.chars().collect::<String>()).collect::<Vec<String>>();
 
-    let mut cost = 0;
-    for pattern in patterns{
-        let chunks = pattern.split("#");
-        let mut sorted_chunks:Vec<String> = vec![];
-        for chunk in chunks{
-            let mut c = chunk.chars().collect::<Vec<char>>();
-            c.sort();
-            sorted_chunks.push(c.into_iter().collect::<String>());
-        }
-        let c =sorted_chunks.join("#").chars().collect::<Vec<char>>();
-        let mut x = c.len();
-        while x > 0{
-            x -=1;
-
-            if c[x] != 'O' {continue;}
-            
-            cost += x+1;
-        }
-
-    }
-    println!("{:?}",cost);
+    let patterns = tilt(lines);
+    
+    println!("{:?}",cost(&patterns));
 }
 
 fn task_2(input: &str){
@@ -80,13 +53,10 @@ fn task_2(input: &str){
             lines = mem.get(&key).unwrap().to_owned(); 
 
             let mapkey = get_key(&lines);
-            if map.contains_key(&mapkey){
-                //print!("{}", map.get(&mapkey).unwrap());
-            } else{
+            if !map.contains_key(&mapkey){
                 let c = (i as u8) as char;
                 i+=1;
                 map.insert(mapkey,c);
-                //print!("{}", c);
             }
 
             continue;
@@ -95,56 +65,38 @@ fn task_2(input: &str){
         mem.insert(key,lines.clone());
 
         let mapkey = get_key(&lines);
-        if map.contains_key(&mapkey){
-//            print!("{}", map.get(&mapkey).unwrap());
-        } else{
-            let c = (i as u8) as char;
-            i+=1;
-            map.insert(mapkey,c);
-        }
+        let c = (i as u8) as char;
+        i+=1;
+        map.insert(mapkey,c);
     }
     lines = transpose(lines);
 
-    let mut cost = 0;
-    for line in lines{
-
-        let c = line.chars().collect::<Vec<char>>();
-        let mut x = c.len();
-        while x > 0{
-            x -=1;
-    
-            if c[x] != 'O' {continue;}
-            
-            cost += x+1;
-
-        }
-    }
-    println!("{}",cost);
+    println!("{}",cost(&lines));
 
 }
 
 fn spin_cycle(input: Vec<String>) -> Vec<String>{
-    let mut patterns: Vec<String>;
+    let mut patterns: Vec<String> = input;
 
-    //north
-    patterns = transpose(input);
-    patterns = tilt(patterns);
-
-    //west
-    patterns = transpose(patterns);
-    patterns = tilt(patterns);
-
-
-    //south
-    patterns = transpose(patterns);
-    patterns = tilt(patterns);
-
-
-    //east
-    patterns = transpose(patterns);
-    patterns = tilt(patterns);
-
+    for _ in 0..4{
+        patterns = tilt(patterns);
+    }
     patterns    
+}
+
+fn tilt(patterns:Vec<String>) -> Vec<String> {
+    let mut sorted_patterns: Vec<String> = vec![];
+
+    for pattern in transpose(patterns){
+        let chunks = pattern.split("#");
+        let sorted_chunks:Vec<String> = chunks
+            .map(|chunk| {chunk.chars().sorted().into_iter().collect::<String>()})
+            .collect::<Vec<String>>();
+
+        sorted_patterns.push(sorted_chunks.join("#"));
+    }
+
+    sorted_patterns
 }
 
 fn transpose(lines:Vec<String>)->Vec<String>{
@@ -164,24 +116,10 @@ fn transpose(lines:Vec<String>)->Vec<String>{
     patterns
 }
 
-fn tilt(patterns:Vec<String>) -> Vec<String> {
-    let mut sorted_patterns: Vec<String> = vec![];
-
-    for pattern in patterns{
-        let chunks = pattern.split("#");
-        let mut sorted_chunks:Vec<String> = vec![];
-        for chunk in chunks{
-            let mut c = chunk.chars().collect::<Vec<char>>();
-            c.sort();
-            sorted_chunks.push(c.into_iter().collect::<String>());
-        }
-        sorted_patterns.push(sorted_chunks.join("#"));
-    }
-
-    sorted_patterns
-
+fn cost(lines: &Vec<String>) -> usize{
+    lines.iter().map(|line| line.chars().enumerate().map(|(i,c)| if c == 'O' { i+1 } else {0}   ).sum::<usize>() ).sum()
 }
 
 fn get_key(pattern: &Vec<String>) -> String{
-       format!("{:?}",pattern)
-   }
+    format!("{:?}",pattern)
+}
