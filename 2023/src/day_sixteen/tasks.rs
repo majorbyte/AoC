ba1b453755d1b91301ab74e91b5a185c5daf283d
majorbyte <majorbyte@gmail.com>
2023-12-16 11:41:17 +0100

    Day 16 cleanup

--- 2023/src/day_sixteen/tasks.rs
+++ 2023/src/day_sixteen/tasks.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -50,16 +50,7 @@ pub fn task() {
 fn task_1(input: &str){
     let mut grid: Vec<Vec<Node>> = input.lines().enumerate().map(|(y,l)| l.chars().enumerate().map(|(x,c)| Node{c,x,y,e:false} ).collect_vec()).collect_vec();
 
-    
-    walk(0,0,0,&mut grid);
-
-    let mut sum = 0;
-    for line in &grid{
-        for node in line{
-            if node.e {sum+=1}
-        }
-    }
-    println!("task 1: {}",sum);   
+    println!("task 1: {}",    walk(0,0,0,&mut grid));   
 }
 
 fn task_2(input: &str){
@@ -69,16 +60,13 @@ fn task_2(input: &str){
     let mut sum = 0;
 
     for x in 0..grid[0].len(){
-        println!("x {}",x);
         let s = walk(x, 0,1,&mut grid);
         if s > sum {sum = s;}
         de_energize(&mut grid);
-        print!("a");
         let s = walk(x, grid.len()-1,3,&mut grid);
         if s > sum {sum = s;}
         de_energize(&mut grid);
     }
-    println!("x");
     for y in 0..grid.len(){
         let s = walk(0, y,0,&mut grid);
         if s > sum {sum = s;}
@@ -88,7 +76,7 @@ fn task_2(input: &str){
         de_energize(&mut grid);
     }
 
-    println!("{}",sum);
+    println!("task 2: {}",sum);
 }
 
 fn de_energize(grid: &mut Vec<Vec<Node>>){
@@ -102,17 +90,17 @@ fn de_energize(grid: &mut Vec<Vec<Node>>){
     
 }
 
-fn walk(s_x:usize,s_y:usize, d:u8, grid: &mut Vec<Vec<Node>>) -> u32 {    
-        let mut to_visit: VecDeque<((usize,usize), u8)> = VecDeque::new();
+fn walk(s_x:usize,s_y:usize, d:u8, grid: &mut Vec<Vec<Node>>) -> usize {    
+        let mut to_visit: VecDeque<(usize,usize, u8)> = VecDeque::new();
         let mut visited: Vec<(usize,usize, u8)> = vec![];
-        to_visit.push_front(((s_x,s_y), d));
+        to_visit.push_front((s_x,s_y, d));
     
         let mut item: Option<_> = to_visit.pop_back();
         while item.is_some() || to_visit.len()>0 {
             to_visit.make_contiguous();
     
-            let (current_node,direction) = item.unwrap();
-            let (mut x, mut y) = (current_node.0, current_node.1);
+            let current_node = item.unwrap();
+            let (mut x, mut y, direction) = (current_node.0, current_node.1, current_node.2);
     
             if visited.contains(&(x,y,direction)) && grid[y][x].e {
                 item = to_visit.pop_back();
@@ -120,135 +108,87 @@ fn walk(s_x:usize,s_y:usize, d:u8, grid: &mut Vec<Vec<Node>>) -> u32 {
             }
             visited.push((x,y,direction));
     
+            let (mut right, mut down, mut left, mut up) = (false,false,false,false);
             match direction{
-                1 => {
-                    while y < grid.len(){
+                1 => { // down
+                    while y < grid.len() && !left && !right{
                         grid[y][x].energized();
                         let node = &grid[y][x];
-                        match node.c {
-                            '-' => {
-                                if node.x > 0 {
-                                    to_visit.push_front(((x-1,y),2));
-                                }
-                                if node.x < grid[0].len()-1{
-                                    to_visit.push_front(((x+1,y),0));
-                                }
-                                break;
-                            },
-                            '/' => {
-                                if node.x >0{
-                                    to_visit.push_front(((x-1,y),2));
-                                }
-                                break;
-                            },
-                            '\\' => {
-                                if node.x < grid[0].len()-1 {
-                                    to_visit.push_front(((x+1,y),0));
-                                }
-                                break;
-                            },
-            
-                            _ => ()
-                        }
+                        (left,right) = match node.c {
+                            '-' => (node.x >0, node.x <grid[0].len()-1),
+                            '/' => (node.x >0,false),
+                            '\\' => (false,node.x < grid[0].len()-1),
+                            
+                            _ => (false,false)
+                        };
+                        if left  {to_visit.push_front((x-1,y,2));}
+                        if right {to_visit.push_front((x+1,y,0));}
                         y+=1;
                     }
-                },           
-                3 => {
+                },      
+                3 => { //Up
                     y+=1;
-                    while y >0{
+                    while y >0 && !left && !right{
                         y-=1;
                         grid[y][x].energized();
                         let node = &grid[y][x];
-                        match node.c {
-                            '-' => {
-                                if node.x > 0 {
-                                    to_visit.push_front(((x-1,y),2));
-                                }
-                                if node.x < grid[0].len()-1{
-                                    to_visit.push_front(((x+1,y),0));
-                                }
-                                break;
-                            },
-                            '/' => {
-                                if node.x < grid[0].len() -1{
-                                    to_visit.push_front(((x+1,y),0));
-                                }
-                                break;
-                            },
-                            '\\' => {
-                                if node.x >0{
-                                    to_visit.push_front(((x-1,y),2));
-                                }
-                                break;
-                            },
+                        (left,right) = match node.c {
+                            '-' => (node.x > 0 , node.x < grid[0].len()-1),
+                            '/' => (false , node.x < grid[0].len() -1),
+                            '\\' => (node.x >0,false),
             
-                            _ => ()
-                        }
-                    }
-    
-                },
-                2 => {
-                    x+=1;
-                    while x >0{
-                        x-=1;
-                        grid[y][x].energized();
-                        let node = &grid[y][x];
-                        match node.c {
-                            '|' => {
-                                if node.y > 0 {
-                                    to_visit.push_front(((x,y-1),3));
-                                }
-                                if node.y < grid.len()-1{
-                                    to_visit.push_front(((x,y+1),1));
-                                }
-                                break;
-                            },
-                            '/' => {
-                                if node.y < grid.len()-1{
-                                    to_visit.push_front(((x,y+1),1));
-                                }
-                                break;
-                            },
-                            '\\' => {
-                                if node.y > 0 {
-                                    to_visit.push_front(((x,y-1),3));
-                                }
-                                break;
-                            },
-            
-                            _ => ()
-                        }
+                            _ => (false,false)
+                        };
+                        if left  {to_visit.push_front((x-1,y,2));}
+                        if right {to_visit.push_front((x+1,y,0));}
                     }
                 },
-                0 => {
-                    while x < grid[0].len(){
+                2 => { // Left
+                    for x in (0..x+1).rev(){
                         grid[y][x].energized();
                         let node = &grid[y][x];
-                        match node.c {
-                            '|' => {
-                                if node.y > 0 {
-                                    to_visit.push_front(((x,y-1), 3));
-                                }
-                                if node.y < grid.len()-1{
-                                    to_visit.push_front(((x,y+1),1));
-                                }
-                                break;
-                            },
-                            '/' => {
-                                if node.y > 0 {
-                                    to_visit.push_front(((x,y-1),3));
-                                }
-                                break;
-                            },
-                            '\\' => {
-                                if node.y < grid.len()-1{
-                                    to_visit.push_front(((x,y+1),1));
-                                }
-                                break;
-                            },
+
+                        (up,down) = match node.c {
+                            '|' => (node.y > 0, node.y < grid.len()-1),
+                            '/' => //(false, node.y < grid.len()-1), // If I use this instead of the below, I get the wrong answer
+                                {
+                                    if node.y < grid.len()-1{
+                                        to_visit.push_front((x,y+1,1));
+                                    }
+                                    break;
+                                },
+                            '\\' => //(node.y > 0 , false),/*{
+                                {
+                                    if node.y > 0 {
+                                        to_visit.push_front((x,y-1,3));
+                                     }
+                                    break;
+                                },
+                            
             
-                            _ => ()
-                        }
+                            _ => (false,false)
+                        };
+
+                        if down {to_visit.push_front((x,y+1, 1));}
+                        if  up  {to_visit.push_front((x,y-1, 3));}
+                        if down | up {break}
+                   }
+                },
+                0 => { // Right
+                    while x < grid[0].len() && !up && !down{
+                        grid[y][x].energized();
+                        let node = &grid[y][x];
+
+                        (up, down) = match node.c {
+                            '|' =>  ( node.y > 0, node.y < grid.len()-1),
+                            '/' => (node.y > 0, false),
+                            '\\' => (false, node.y < grid.len()-1),
+            
+                            _ => (false,false)
+                        };
+                         if  up  {to_visit.push_front((x,y-1, 3));}
+                         if down {to_visit.push_front((x,y+1, 1));}
+
                         x+=1;
                     }
             
@@ -258,11 +198,5 @@ fn walk(s_x:usize,s_y:usize, d:u8, grid: &mut Vec<Vec<Node>>) -> u32 {
             item = to_visit.pop_back();
         }
     
-        let mut sum = 0;
-        for line in grid{
-            for node in line{
-                if node.e {sum+=1}
-            }
-        }
-    sum
+    grid.iter().map(|l| l.iter().filter(|n| n.e).count()).sum()
 }
